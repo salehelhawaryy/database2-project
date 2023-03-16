57d84ae20d3312264f051b392e20a7a4c9047997
Youssef Tamer <yousseftamer1990@gmail.com>
2023-03-16 23:18:44 +0200

    Added pages

--- src/Page.java
+++ src/Page.java
@@ -1,8 +1,19 @@
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Vector;
+import java.io.Serializable;
 
-public class Page implements Serializable {
-	Vector<Hashtable<String, String>> tuples; 
+public class Page implements java.io.Serializable {
+	int maximumRows;
+	Vector<Vector<Hashtable<Object,Object>>> tuples;
+	public Page(int maximumRows) {
+		this.maximumRows=maximumRows;
+	}
+
+	public int getMaximumRows() {
+		return maximumRows;
+	}
+
+	public void setMaximumRows(int maximumRows) {
+		this.maximumRows = maximumRows;
+	}
 }
