5e8926925740c2999a7ac36e38579d9ae967d080
Youssef Tamer <yousseftamer1990@gmail.com>
2023-03-21 23:42:06 +0200

    made sorting in pages

--- src/DBApp.java
+++ src/DBApp.java
@@ -7,7 +7,17 @@ import java.util.Vector;
 
 public class DBApp {
 	Vector<Table> tables;
-	
+	String MaximumRowsCountinTablePage;
+	String MaximumEntriesinOctreeNode;
+	public DBApp() throws IOException {
+		File f =new File("resources/DBApp.config");
+		FileInputStream fis = new FileInputStream(f);
+		Properties p = new Properties();
+		p.load(fis);
+		 MaximumRowsCountinTablePage = p.getProperty("MaximumRowsCountinTablePage");
+		 MaximumEntriesinOctreeNode = p.getProperty("MaximumEntriesinOctreeNode");
+	}
+
 	public void createTable(String tableName,String clusteringKey, Hashtable<String,String> ColNameType,  
 		Hashtable<String, String> ColNameMin,Hashtable<String,String> ColNameMax) throws DBAppException
 	{
@@ -34,20 +44,19 @@ public class DBApp {
 		}
 
 		if(table.rows.isEmpty()) {
-			Page page = new Page(3);
+			Page page = new Page(Integer.parseInt(MaximumRowsCountinTablePage));
 			table.rows.add(page);
 		}
 
 		String pk = table.getPK();
-		htblColNameValue.get(pk);
-
-
 
 		for(Page p : table.rows ) {
 			int count = 0;
 			for(Hashtable<String,Object> tuple : p.tuples) {
+
 				if(tuple.get(pk).toString().compareTo(htblColNameValue.get(pk).toString()) > 0) {
 					p.tuples.insertElementAt(htblColNameValue, count);
+					redistributeIns(table);
 				}
 				count ++;
 			}
@@ -55,6 +64,28 @@ public class DBApp {
 
 	}
 
+	public void redistributeIns(Table table) {
+		for(int i=0;i<table.rows.size();i++)
+		{
+//			Vector<Page> v1 = table.rows;
+//			Vector<Hashtable<String,Object>> v2= v1.get(i).tuples;
+			if(table.rows.get(i).tuples.size()>Integer.parseInt(MaximumRowsCountinTablePage))
+			{
+				if(i==table.rows.size()-1)
+				{
+					Page page = new Page(Integer.parseInt(MaximumRowsCountinTablePage));
+					table.rows.add(page);
+					page.tuples.add(table.rows.get(i).tuples.get(table.rows.get(i).tuples.size()-1));
+					table.rows.get(i).tuples.remove(table.rows.get(i).tuples.size()-1);
+				}
+				else
+				{
+					table.rows.get(i+1).tuples.insertElementAt(table.rows.get(i).tuples.get(table.rows.get(i).tuples.size()-1),0);
+					table.rows.get(i).tuples.remove(table.rows.get(i).tuples.size()-1);
+				}
+			}
+		}
+	}
 
 
 	public static void main(String[] args) throws IOException {
